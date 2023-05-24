3f6388ab0b339beea02b2c6fcf98cd95778c9b97
AbhiAdhav007 <120732315+AbhiAdhav007@users.noreply.github.com>
2023-05-24 17:04:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-let team = players;
+const team = players;
+const team1 = players.slice();
 
-team1 = players.copy()
+//let copiedPerson = Object.assign({}, person);
+let cap1 = person;
 
-// Create a copy of the person object
-cap1 = dict(person)
+
 window.players = players;
 window.person = person;
 window.team = team;
 window.team1 = team1;
-window.cap1 = cap1;
